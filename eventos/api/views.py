1ecad892005ef94d3ae8f0a356ba50e27a1a96d5
Vukknesh <leomcn@hotmail.com>
2020-01-12 00:28:15 -0400

    a

--- eventos/api/views.py
+++ eventos/api/views.py
@@ -97,10 +97,9 @@ class EventoUpdateAPIView(UpdateAPIView):
             print(f'now.hour {now.hour}')
             print(f'now.day {now.day}')
             print(f'evento.starting_date.day {evento.starting_date.day}')
-            print(
-                f'((now.day - evento.starting_date.day).days == -1) {((evento.starting_date.day - now.day).days == 1)}')
             print('b acima do if')
-            if(now.hour > 20 and ((evento.starting_date.day - now.day).days == 1)):
+            if(now.hour > 20 and (evento.starting_date.day - now.day == 1)):
+                print(((evento.starting_date.day - now.day).days == 1))
                 print('b')
                 raise ValidationError(
                     {"message": "Voce so pode remarcar aulas matutinas antes das 20hrs do dia anterior."})
