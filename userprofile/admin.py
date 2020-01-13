e964589ac49b754e44f9e1b268622f0baa070fd9
Vukknesh <leomcn@hotmail.com>
2020-01-13 06:48:06 -0400

    a

--- userprofile/admin.py
+++ userprofile/admin.py
@@ -4,9 +4,10 @@ from .models import Profile
 # Register your models here.
 # admin.site.register(Profile)
 
-admin.site.register(Profile, MyAdmin)
-
 
 class MyAdmin(admin.ModelAdmin):
     def has_add_permission(self, request, obj=None):
         return False
+
+
+admin.site.register(Profile, MyAdmin)
