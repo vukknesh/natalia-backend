84889ead5b6b32f2fa85a19d917ea25293fb3d9c
Vukknesh <leomcn@hotmail.com>
2020-09-11 21:09:52 -0300

    a

--- eventos/api/views.py
+++ eventos/api/views.py
@@ -201,8 +201,8 @@ class EventoListAPIView(ListAPIView):
         dia_pg = u.profile.dia_pagamento
         month_mais = month + 1
 
-        start_date = datetime.date(year, month, dia_pg)
-        end_date = datetime.date(year, month_mais, dia_pg)
+        start_date = datetime.datetime(year, month, dia_pg)
+        end_date = datetime.datetime(year, month_mais, dia_pg)
         print(f'month  {month}')
         print(f'dia_pg mais {dia_pg}')
         # qs = Evento.objects.filter(starting_date__gte=datetime.now(), starting_date__year__gte=year,
@@ -212,7 +212,7 @@ class EventoListAPIView(ListAPIView):
                            starting_date__month__gte=month, starting_date__day__gte=dia_pg, starting_date__year__lte=year, starting_date__month__lte=month_mais, starting_date__day__lt=dia_pg)
         print(f'queryset_list === {queryset_list}')
         qs = Evento.objects.filter(user=u).filter(
-            starting_date__gte=start_date, starting_date__lte=end_date)
+            starting_date__date__gte=start_date, starting_date__date__lte=end_date)
         for qqq in qs:
             print(f'qqq.starting_date.day = {qqq.starting_date.day}')
         print(f'qs === {qs}')
