424b44382e8ec88740e3c3790162c5605f1884c8
Vukknesh <leomcn@hotmail.com>
2020-09-15 18:16:41 -0300

    a

--- userprofile/scheduler.py
+++ userprofile/scheduler.py
@@ -3,6 +3,7 @@ from datetime import datetime, date, timedelta
 import os
 import time
 from .models import Profile
+from django.utils import timezone
 
 from apscheduler.schedulers.background import BackgroundScheduler
 from django_apscheduler.jobstores import DjangoJobStore, register_events, register_job
@@ -12,12 +13,35 @@ scheduler = BackgroundScheduler()
 scheduler.add_jobstore(DjangoJobStore(), "default")
 
 
-# @register_job(scheduler, "interval", seconds=30, replace_existing=True)
-# @register_job(scheduler, 'interval', seconds=30, replace_existing=True)
+@register_job(scheduler, "interval", seconds=60, replace_existing=True)
+def testeeeees():
+    now = datetime.now(timezone.utc)
+
+    alunos = Profile.objects.all()
+    for aluno in alunos:
+        print(f'aluno.dia_pagamento = {aluno.dia_pagamento}')
+        print(f'now.day = {now.day}')
+
+        if aluno.dia_pagamento == now.day:
+            print(f'aluno.dia_pagamento = {aluno.dia_pagamento}')
+            Profile.objects.filter(user=aluno.user).update(aulas_remarcadas=0)
+
+
+@register_job(scheduler, trigger='cron', hour='23', minute='59', replace_existing=True)
+def resetRemarcadas():
+    now = datetime.now(timezone.utc)
+
+    alunos = Profile.objects.all()
+    for aluno in alunos:
+        if aluno.dia_pagamento == now.day:
+            Profile.objects.filter(user=aluno.user).update(aulas_remarcadas=0)
+
+    # Profile.objects.all().update(aulas_remarcadas=0)
+
+
 @register_job(scheduler, trigger='cron', day='last', hour='20', replace_existing=True)
 def resetDia():
-    Profile.objects.all().update(aulas_remarcadas=0)
-    print("Profile done")
+
     resumo_mensal()
 
 
