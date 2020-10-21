b510fc8d3f4409dc292e5d9b397f579adb70f939
Vukknesh <leomcn@hotmail.com>
2020-10-21 18:05:17 -0300

    a

--- eventos/api/views.py
+++ eventos/api/views.py
@@ -665,9 +665,9 @@ def repor_aula(request):
     if aulas_do_mes.count() > aluno_reposicao:
         print(f'dentro do count > aluno_reposicao')
         # verificar se a data selecionada esta no mes atual do usuario
-        print(f'data.date() = {data.date()}')
-        print(f'start_date.date() = {start_date.date()}')
-        if data.date() > start_date.date() and data.date() < end_date.date():
+        print(f'data.date() = {data}')
+        print(f'start_date.date() = {start_date}')
+        if data > start_date and data < end_date:
             print(f'dentro do data do periodo do aluno')
             # verificar se existe aula nesse horario e no dia
             if Evento.objects.filter(starting_date__gte=now, starting_date__lt=end_date).exists():
