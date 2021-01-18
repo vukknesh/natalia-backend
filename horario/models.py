1b7821391500ee1c2d149615bc1ab89a55acabb9
Vukknesh <leomcn@hotmail.com>
2021-01-18 16:53:01 -0300

    a

--- horario/models.py
+++ horario/models.py
@@ -25,8 +25,7 @@ class Horario(models.Model):
     )
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
-    dia = models.IntegerField(
-        choices=DIA_CHOICES, default=SEGUNDA, null=True, blank=True)
+    dia = models.IntegerField(choices=DIA_CHOICES, default=0)
     hora_aula = models.TimeField(
         auto_now=False, auto_now_add=False, default=timezone.now)
 
