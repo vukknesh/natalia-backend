950b25a0d117b07e0739294a2666b7b75d737982
Vukknesh <leomcn@hotmail.com>
2021-01-28 15:24:07 -0300

    a

--- horariod/admin.py
+++ horariod/admin.py
@@ -4,4 +4,13 @@ from .models import Horario
 # Register your models here.
 
 
-admin.site.register(Horario)
+class ExtraAdmin(admin.ModelAdmin):
+    list_display = ('get_name', 'weekday', 'hora_aula')
+    list_editable = ('weekday', 'hora_aula')
+    list_filter = ('weekday', 'user', 'hora_aula')
+
+    def get_name(self, obj):
+        return "{}".format(obj.user.first_name)
+
+
+admin.site.register(Horario, ExtraAdmin)
