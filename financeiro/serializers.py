3cbf93debf4c2a558ac139320f5fbcd1534d2b6d
Vukknesh <leomcn@hotmail.com>
2021-11-08 10:51:45 -0400

    a

--- financeiro/serializers.py
+++ financeiro/serializers.py
@@ -90,17 +90,17 @@ class DespesasSerializer(ModelSerializer):
 
 class ResumoManualMesListAllSerializer(ModelSerializer):
     despesas_do_mes = DespesasSerializer(many=True, read_only=True)
-    total = SerializerMethodField()
+    # total = SerializerMethodField()
 
-    def get_total(self, obj):
-        t = 0
-        print(f'get Total ')
-        print(f'self {self} ')
-        print(f'self.despesas_set.all() {self.despesas_set.all()} ')
-        for a in self.despesas_set.objects.all():
-            t += a.valor
+    # def get_total(self, obj):
+    #     t = 0
+    #     print(f'get Total ')
+    #     print(f'self {self} ')
+    #     print(f'self.despesas_set.all() {self.despesas_set.all()} ')
+    #     for a in self.despesas_set.objects.all():
+    #         t += a.valor
 
-        return t
+    #     return t
 
     class Meta:
         model = ResumoManualMes
